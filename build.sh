f769ea0de19789987ce3c1c47147364ead7deac7
Tony Hesjevik <tony@hesjevik.no>
2015-04-23 20:07:50 +0200

    Make versions

--- build.sh
+++ build.sh
@@ -1,54 +1,100 @@
 #!/usr/bin/env bash
-
-set -x              # Print command traces before executing command
+set -x
 trap 'exit 1' ERR   # Exit script with error if command fails
 
-cd $(dirname "${BASH_SOURCE[0]}")
-
 if [[ -z $(which docker) ]]; then
-    echo "Missing docker client which is required for building, testing and pushing."
-    exit 2
+    echo "Missing docker client which is required for building, testing and pushing"
+    exit 3
 fi
 
+declare PROJECT_DIRECTORY=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+declare DOCKER_IMAGE_NAME="gainmaster/nodejs"
+declare VERSION_DIRECTORY="${PROJECT_DIRECTORY}/version"
 
-declare IMAGE_NAME="gainmaster/nodejs"
+cd $PROJECT_DIRECTORY
 
 
 function build {
-    docker build -t $IMAGE_NAME .
+    version="${1}"
+    directory="${VERSION_DIRECTORY}/${version}"
+
+    if [ ! -d "$directory" ]; then
+        echo "Cant build image ${DOCKER_IMAGE_NAME}:${version}, the verision does not exist"
+        exit 1
+    fi
+
+    while read tag; do
+        docker build -t "${DOCKER_IMAGE_NAME}:${tag}" "${directory}"
+    done < "${directory}/tags"
 }
 
 
 function test {
+    version="${1}"
+    directory="${VERSION_DIRECTORY}/${version}"
 
-	docker history $IMAGE_NAME 2> /dev/null
+    if [ ! -d "$directory" ]; then
+        echo "Cant test image ${DOCKER_IMAGE_NAME}:${version}, the verision does not exist"
+        exit 1
+    fi
+
+    docker history "${DOCKER_IMAGE_NAME}:${version}" 2> /dev/null
+
     if [ $? -eq 1 ]; then
-        echo "Cant test $IMAGE_NAME, the image does not exist."
+        echo "Cant test ${DOCKER_IMAGE_NAME}:${version}, the image is not built"
         exit 2
     fi
-    
-    if [[ -z $(which bats) ]]; then
-        echo "Missing bats, which is required for testing."
+
+    #test/${version}.bats
+}
+
+
+function run {
+    version="${1}"
+    directory="${VERSION_DIRECTORY}/${version}"
+
+    if [ ! -d "$directory" ]; then
+        echo "Cant test image ${DOCKER_IMAGE_NAME}:${version}, the verision does not exist"
+        exit 1
+    fi
+
+    docker history "${DOCKER_IMAGE_NAME}:${version}" 2> /dev/null
+
+    if [ $? -eq 1 ]; then
+        echo "Cant run ${DOCKER_IMAGE_NAME}:${version}, the image is not built"
         exit 2
     fi
-    
-    bats test/
+
+    docker run --rm -it "${DOCKER_IMAGE_NAME}:${version}" bash
 }
 
 
 function push {
-	docker history $IMAGE_NAME 2> /dev/null
+    version="${1}"
+    directory="${VERSION_DIRECTORY}/${version}"
 
-    if [ $? -eq 1 ]; then
-        echo "Cant push $IMAGE_NAME, the image does not exist."
-        exit 2
+    if [ ! -d "$directory" ]; then
+        echo "Cant push image ${DOCKER_IMAGE_NAME}:${version}, the verision does not exist"
+        exit 1
     fi
 
-    [ -z "$DOCKER_EMAIL" ]    && { echo "Need to set DOCKER_EMAIL";    exit 1; }
-    [ -z "$DOCKER_USER" ]     && { echo "Need to set DOCKER_USER";     exit 1; }
-    [ -z "$DOCKER_PASSWORD" ] && { echo "Need to set DOCKER_PASSWORD"; exit 1; }
+    docker history "${DOCKER_IMAGE_NAME}:${version}" 2> /dev/null
+
+    if [ $? -eq 1 ]; then
+        echo "Cant push ${DOCKER_IMAGE_NAME}:${version}, the image is not built."
+        exit 3
+    fi
+
+    [ -z "$DOCKER_EMAIL" ]    && { echo "Need to set DOCKER_EMAIL";    exit 4; }
+    [ -z "$DOCKER_USER" ]     && { echo "Need to set DOCKER_USER";     exit 4; }
+    [ -z "$DOCKER_PASSWORD" ] && { echo "Need to set DOCKER_PASSWORD"; exit 4; }
 
     if [[ $EUID -ne 0 ]]; then
+        if [[ -z $(which sudo) ]]; then
+            echo "Missing sudo client which is required for pushing when not root"
+            exit 2
+        fi
+
         sudo docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWORD
         sudo docker push $IMAGE_NAME
     else
@@ -61,25 +107,54 @@ function push {
 #
 # Handle input
 #
-
+versions=()
 actions=("$@")
+
+while getopts ":v:" opt; do
+  case $opt in
+    v)
+      versions+=("$OPTARG")
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG"
+      ;;
+  esac
+done
 
 if [ ${#actions[@]} -eq 0 ]; then
     actions=(build test push)
 fi
 
+if [ ${#versions[@]} -eq 0 ]; then
+    for version in ${VERSION_DIRECTORY}/*; do
+        versions+=(nodejs bower-grunt yeoman generator-angular generator-cg-angular)
+    done
+fi
+
 for action in "${actions[@]}"; do 
     case "$action" in
         build)
-            build
+            for version in "${versions[@]}"; do
+               build $version
+            done
             ;;
          
         test)
-            test
+            for version in "${versions[@]}"; do
+               test $version
+            done
+            ;;
+
+        run)
+            for version in "${versions[@]}"; do
+               run $version
+            done
             ;;
 
         push)
-            push
+            for version in "${versions[@]}"; do
+               push $version
+            done
             ;;
     esac
 done
